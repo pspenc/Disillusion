b1eb33a5067b870bd7ae857aa0551eef467367aa
pspenc <31780769+pspenc@users.noreply.github.com>
2020-10-04 21:47:57 -0600

    initial commit baby

---
+++ Disillusion/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("References")]
+    [Tooltip("Reference to the main camera used for the player")]
+    public Camera playerCamera;
+    [Tooltip("Audio source for footsteps, jump, etc...")]
+    public AudioSource audioSource;
+
+    [Header("General")]
+    [Tooltip("Force applied downward when in the air")]
+    public float gravityDownForce = 20f;
+    [Tooltip("Physic layers checked to consider the player grounded")]
+    public LayerMask groundCheckLayers = -1;
+    [Tooltip("distance from the bottom of the character controller capsule to test for grounded")]
+    public float groundCheckDistance = 0.05f;
+
+    [Header("Movement")]
+    [Tooltip("Max movement speed when grounded (when not sprinting)")]
+    public float maxSpeedOnGround = 10f;
+    [Tooltip("Sharpness for the movement when grounded, a low value will make the player accelerate and decelerate slowly, a high value will do the opposite")]
+    public float movementSharpnessOnGround = 15;
+    [Tooltip("Max movement speed when crouching")]
+    [Range(0, 1)]
+    public float maxSpeedCrouchedRatio = 0.5f;
+    [Tooltip("Max movement speed when not grounded")]
+    public float maxSpeedInAir = 10f;
+    [Tooltip("Acceleration speed when in the air")]
+    public float accelerationSpeedInAir = 25f;
+    [Tooltip("Multiplicator for the sprint speed (based on grounded speed)")]
+    public float sprintSpeedModifier = 2f;
+    [Tooltip("Height at which the player dies instantly when falling off the map")]
+    public float killHeight = -50f;
+
+    [Header("Rotation")]
+    [Tooltip("Rotation speed for moving the camera")]
+    public float rotationSpeed = 200f;
+    [Range(0.1f, 1f)]
+    [Tooltip("Rotation speed multiplier when aiming")]
+    public float aimingRotationMultiplier = 0.4f;
+
+    [Header("Jump")]
+    [Tooltip("Force applied upward when jumping")]
+    public float jumpForce = 9f;
+
+    [Header("Stance")]
+    [Tooltip("Ratio (0-1) of the character height where the camera will be at")]
+    public float cameraHeightRatio = 0.9f;
+    [Tooltip("Height of character when standing")]
+    public float capsuleHeightStanding = 1.8f;
+    [Tooltip("Height of character when crouching")]
+    public float capsuleHeightCrouching = 0.9f;
+    [Tooltip("Speed of crouching transitions")]
+    public float crouchingSharpness = 10f;
+
+    [Header("Audio")]
+    [Tooltip("Amount of footstep sounds played when moving one meter")]
+    public float footstepSFXFrequency = 1f;
+    [Tooltip("Amount of footstep sounds played when moving one meter while sprinting")]
+    public float footstepSFXFrequencyWhileSprinting = 1f;
+    [Tooltip("Sound played for footsteps")]
+    public AudioClip footstepSFX;
+    [Tooltip("Sound played when jumping")]
+    public AudioClip jumpSFX;
+    [Tooltip("Sound played when landing")]
+    public AudioClip landSFX;
+    [Tooltip("Sound played when taking damage froma fall")]
+    public AudioClip fallDamageSFX;
+
+    [Header("Fall Damage")]
+    [Tooltip("Whether the player will recieve damage when hitting the ground at high speed")]
+    public bool recievesFallDamage;
+    [Tooltip("Minimun fall speed for recieving fall damage")]
+    public float minSpeedForFallDamage = 10f;
+    [Tooltip("Fall speed for recieving th emaximum amount of fall damage")]
+    public float maxSpeedForFallDamage = 30f;
+    [Tooltip("Damage recieved when falling at the mimimum speed")]
+    public float fallDamageAtMinSpeed = 10f;
+    [Tooltip("Damage recieved when falling at the maximum speed")]
+    public float fallDamageAtMaxSpeed = 50f;
+
+
+
+
+
+    public Vector3 characterVelocity { get; set; }
+
+
+
+    PlayerInputHandler playerInputHandler;
+    CharacterController m_Controller;
+
+    Vector3 m_GroundNormal;
+    Vector3 m_CharacterVelocity;
+    Vector3 m_LatestImpactSpeed;
+    float m_LastTimeJumped = 0f;
+    float m_CameraVerticalAngle = 0f;
+    float m_footstepDistanceCounter;
+    float m_TargetCharacterHeight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Controller = GetComponent<CharacterController>();
+
+
+        playerInputHandler = GetComponent<PlayerInputHandler>();
+
+        m_Controller.enableOverlapRecovery = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        HandleCharacterMovement();
+    }
+
+
+
+
+
+
+
+
+
+
+    void HandleCharacterMovement()
+    {
+
+         // horizontal character rotation
+        {
+            // rotate the transform with the input speed around its local Y axis
+            transform.Rotate(new Vector3(0f, (playerInputHandler.GetLookInputsHorizontal() * rotationSpeed * 1), 0f), Space.Self);
+        }
+
+        // vertical camera rotation
+        {
+            // add vertical inputs to the camera's vertical angle
+            m_CameraVerticalAngle += playerInputHandler.GetLookInputsVertical() * rotationSpeed * 1;
+
+            // limit the camera's vertical angle to min/max
+            m_CameraVerticalAngle = Mathf.Clamp(m_CameraVerticalAngle, -89f, 89f);
+
+            // apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
+            playerCamera.transform.localEulerAngles = new Vector3(m_CameraVerticalAngle, 0, 0);
+        }
+
+
+        {
+            Vector3 worldspaceMoveInput = transform.TransformVector(playerInputHandler.GetMoveInput());
+
+            Vector3 targetVeloctiy = worldspaceMoveInput * maxSpeedOnGround;
+            characterVelocity = Vector3.Lerp(characterVelocity, targetVeloctiy, movementSharpnessOnGround * Time.deltaTime);
+
+
+            //do footsteps here later
+
+        }
+
+
+        Vector3 capsuleBottomBeforeMove = GetCapsuleBottomHemisphere();
+        Vector3 capsuleTopBeforeMove = GetCapsuleTopHemisphere(m_Controller.height);
+
+        m_Controller.Move(characterVelocity * Time.deltaTime);
+
+        m_LatestImpactSpeed = Vector3.zero;
+        if (Physics.CapsuleCast(capsuleBottomBeforeMove, capsuleTopBeforeMove, m_Controller.radius, characterVelocity.normalized,
+            out RaycastHit hit, characterVelocity.magnitude * Time.deltaTime, -1, QueryTriggerInteraction.Ignore))
+        {
+
+            m_LatestImpactSpeed = characterVelocity;
+            characterVelocity = Vector3.ProjectOnPlane(characterVelocity, hit.normal);
+        }
+
+
+    }
+
+
+    // Gets the center point of the bottom hemisphere of the character controller capsule    
+    Vector3 GetCapsuleBottomHemisphere()
+    {
+        return transform.position + (transform.up * m_Controller.radius);
+    }
+
+    // Gets the center point of the top hemisphere of the character controller capsule    
+    Vector3 GetCapsuleTopHemisphere(float atHeight)
+    {
+        return transform.position + (transform.up * (atHeight - m_Controller.radius));
+    }
+
+
+}
---
+++ Disillusion/Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,119 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+    [Tooltip("Sensitivity multiplier for moving the camera around")]
+    public float lookSensitivity = 1f;
+    [Tooltip("Additional sensitivity multiplier for WebGL")]
+    public float webglLookSensitivityMultiplier = 0.25f;
+    [Tooltip("Limit to consider an input when using a trigger on a controller")]
+    public float triggerAxisThreshold = 0.4f;
+    [Tooltip("Used to flip the vertical input axis")]
+    public bool invertYAxis = false;
+    [Tooltip("Used to flip the horizontal input axis")]
+    public bool invertXAxis = false;
+
+
+    
+    
+    PlayerController m_PlayerController;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_PlayerController = GetComponent<PlayerController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    
+    public bool CanProcessInput()
+    {
+        return true;
+    }
+
+    public Vector3 GetMoveInput()
+    {
+
+        //will having something different for when puzzles are open and we dont have movement rather just the puzzle touching
+
+        Vector3 move = new Vector3(Input.GetAxisRaw(GameConstants.k_AxisNameHorizontal), 0f, Input.GetAxisRaw(GameConstants.k_AxisNameVertical));
+
+        move = Vector3.ClampMagnitude(move, 1);
+
+
+        return move;
+    }
+
+
+
+    public float GetLookInputsHorizontal()
+    {
+        return GetMouseOrStickLookAxis(GameConstants.k_MouseAxisNameHorizontal, GameConstants.k_AxisNameJoystickLookHorizontal);
+    }
+
+    public float GetLookInputsVertical()
+    {
+        return -1*GetMouseOrStickLookAxis(GameConstants.k_MouseAxisNameVertical, GameConstants.k_AxisNameJoystickLookVertical);
+    }
+
+    float GetMouseOrStickLookAxis(string mouseInputName, string stickInputName)
+    {
+        if (CanProcessInput())
+        {
+            // Check if this look input is coming from the mouse
+            bool isGamepad = Input.GetAxis(stickInputName) != 0f;
+            float i = isGamepad ? Input.GetAxis(stickInputName) : Input.GetAxisRaw(mouseInputName);
+
+            // handle inverting vertical input
+            if (invertYAxis)
+                i *= -1f;
+
+            // apply sensitivity multiplier
+            i *= lookSensitivity;
+
+            if (isGamepad)
+            {
+                // since mouse input is already deltaTime-dependant, only scale input with frame time if it's coming from sticks
+                i *= Time.deltaTime;
+            }
+            else
+            {
+                // reduce mouse input amount to be equivalent to stick movement
+                i *= 0.01f;
+#if UNITY_WEBGL
+                // Mouse tends to be even more sensitive in WebGL due to mouse acceleration, so reduce it even more
+                i *= webglLookSensitivityMultiplier;
+#endif
+            }
+
+            return i;
+        }
+
+        return 0f;
+    }
+}
