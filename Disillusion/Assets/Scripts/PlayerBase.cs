8a3d1d66f52d4e6241d736dfb044bccb122204fb
wxz97121 <wxz97121@gmail.com>
2020-10-11 19:02:04 +0800

    Fixed Scene button animation. Finished White screen on the max anxiety.

--- Disillusion/Assets/Scripts/PlayerBase.cs
+++ Disillusion/Assets/Scripts/PlayerBase.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class PlayerBase : SingletonBase<PlayerBase>
 {
@@ -8,7 +11,7 @@ public class PlayerBase : SingletonBase<PlayerBase>
     public float anxietyLevel;
     public float maxAnxiety;
     private int frameCount;
-
+    public Image Background;
     public MeterUI meterUI;
     public bool IsBreathing = false;
 
@@ -37,17 +40,29 @@ public class PlayerBase : SingletonBase<PlayerBase>
         anxietyLevel = Mathf.Clamp(anxietyLevel, 0, maxAnxiety);
     }
     // Update is called once per frame
+    bool HasDied=false;
     void Update()
     {
-
+        
         if(IsBreathing)
         {
             ChangeAnxiety(-.01f);
         }
 
-
+        if (Background && !HasDied && Mathf.Approximately(anxietyLevel, maxAnxiety))
+            StartCoroutine(IDie());
         meterUI.UpdateMeter(anxietyLevel);
 
 
+    }
+    IEnumerator IDie()
+    {
+        HasDied = true;
+        transform.GetComponent<PlayerController>().EnableMovement = false;
+        Background.DOFade(1, 1);
+        //TODO Play an Audio
+        yield return new WaitForSeconds(3);
+        
+        SceneManager.LoadScene(0);
     }
 }
